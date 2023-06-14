e055ede0f7ca3bd2c29bde6e65198a89ce71689f
Nzt3 <nzt3pro@gmail.com>
2023-06-14 23:02:30 +0900

    ItemList.vueを作ったところ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
+
+import Clickcounter from './ClickCounter.vue'
+import ItemList from './ItemList.vue'
 defineProps<{
   msg: string
 }>()
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div>
+    <h1 >{{ msg }}</h1>
+    <Clickcounter />
+    <ItemList />
   </div>
 </template>
 
 <style scoped>
+a {
+  color : #42b983;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -26,15 +30,4 @@ h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
