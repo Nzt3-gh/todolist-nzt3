4115af344fa15877383a1d6caf579fbed5aed87a
Nzt3 <nzt3pro@gmail.com>
2023-06-15 15:35:58 +0900

    リスト形式に変更

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,12 +3,15 @@ import {ref} from 'vue'
 interface Task{
   name:string
   done:number
+  id:number
 }
+var number_of_tasks=0
 const tasks=ref<Task[]>([
 ])
 const newTaskName=ref('')
 const addTask = () => {
-  tasks.value.push({name:newTaskName.value,done:0})
+  tasks.value.push({name:newTaskName.value,done:0,id:number_of_tasks})
+  number_of_tasks+=1
 }
 const doTask = (task: Task)=>{
   task.done=1
@@ -22,15 +25,19 @@ const doTask = (task: Task)=>{
     <input v-model="newTaskName" type="text">
     <button @click="addTask">追加</button>
   </div>
-  <div class="ongoing" v-for="task in tasks" :key="task.name">
-    <div v-if="task.done==0">タスク名:{{ task.name }}
-      <button @click="doTask(task)">完了</button>
-    </div>
-  </div>
- 
-  <div class="done" v-for="task in tasks" :key="task.name">
-    <div v-if="task.done==1">タスク名{{ task.name }}</div>
-  </div>
+  <ul>
+    <il class="ongoing" v-for="task in tasks" :key="task.name">
+      <div v-if="task.done==0" class="task">{{ task.id }} {{ task.name }}
+        <button @click="doTask(task)" class="dobutton">完了</button>
+      </div>
+    </il>
+  </ul>
+  
+  <ul>
+    <il class="done" v-for="task in tasks" :key="task.name">
+      <div v-if="task.done==1" class="task">{{ task.id }} {{ task.name }}</div>
+    </il>
+  </ul>
   </div>
 </template>
 <style>
@@ -40,5 +47,10 @@ const doTask = (task: Task)=>{
 .done{
   color:grey;
 }
+.task{
+  margin: auto;
+  padding: 5px;
+}
+
 
 </style>
