7c994f4c772a33191871699377ed4ad0b8a999f7
Nzt3 <nzt3pro@gmail.com>
2023-06-15 15:20:08 +0900

    TodoListを作った

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
-import Clickcounter from './ClickCounter.vue'
-import ItemList from './ItemList.vue'
+import TodoList from './TodoList.vue'
+
 defineProps<{
   msg: string
 }>()
@@ -9,9 +9,7 @@ defineProps<{
 
 <template>
   <div>
-    <h1 >{{ msg }}</h1>
-    <Clickcounter />
-    <ItemList />
+    <TodoList/>
   </div>
 </template>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+interface Task{
+  name:string
+  done:number
+}
+const tasks=ref<Task[]>([
+])
+const newTaskName=ref('')
+const addTask = () => {
+  tasks.value.push({name:newTaskName.value,done:0})
+}
+const doTask = (task: Task)=>{
+  task.done=1
+}
+
+</script>
+<template>
+  <div>
+    <h1>TodoList</h1>
+  <div>
+    <input v-model="newTaskName" type="text">
+    <button @click="addTask">追加</button>
+  </div>
+  <div class="ongoing" v-for="task in tasks" :key="task.name">
+    <div v-if="task.done==0">タスク名:{{ task.name }}
+      <button @click="doTask(task)">完了</button>
+    </div>
+  </div>
+ 
+  <div class="done" v-for="task in tasks" :key="task.name">
+    <div v-if="task.done==1">タスク名{{ task.name }}</div>
+  </div>
+  </div>
+</template>
+<style>
+.ongoing{
+  color: black;
+}
+.done{
+  color:grey;
+}
+
+</style>
